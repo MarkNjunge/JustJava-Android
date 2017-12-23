080328428973b467741df4fc1a9d64a49b7c7167
Mark Njung'e <mark.kamau@outlook.com>
2017-12-23 17:51:10 +0300

    Deleted old test files

--- app/src/test/java/com/marknkamau/justjava/MockitoUtils.kt
+++
@@ -1,7 +0,0 @@
-package com.marknkamau.justjava
-
-import com.marknkamau.justjava.authentication.AuthenticationService
-import org.mockito.Mockito
-
-fun anyAuthActionListener(): AuthenticationService.AuthActionListener? = Mockito.any(AuthenticationService.AuthActionListener::class.java)
-
