b460905018e854f617b70e000bfa7415b037cd36
Mark Njung'e <mark.kamau@outlook.com>
2018-07-25 13:23:28 +0300

    Add firebase messaging

--- app/src/main/java/com/marknkamau/justjava/data/network/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/marknkamau/justjava/data/network/MyFirebaseMessagingService.kt
@@ -3,6 +3,7 @@ package com.marknkamau.justjava.data.network
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.marknkamau.justjava.JustJavaApp
+import timber.log.Timber
 
 /**
  * Created by Mark Njung'e.
@@ -15,11 +16,25 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         remoteMessage.notification?.let {
-            notificationHelper.showNotification(it.title ?: "JustJava" , it.body ?: "")
+            notificationHelper.showNotification(it.title ?: "JustJava", it.body ?: "")
         }
         remoteMessage.data?.let {
-            if(it["reason"] == "completed-order"){
+            Timber.d(it.toString())
+            if (it["reason"] == "completed-order") {
                 notificationHelper.showCompletedOrderNotification("Your order has been completed.")
+            }
+
+            when (it["reason"]) {
+                "completed-order" -> {
+                    notificationHelper.showCompletedOrderNotification("Your order has been completed.")
+                }
+                "mpesa" -> {
+                    it["body"]?.let { body ->
+                        notificationHelper.showNotification("Payment", body)
+                    }
+                }
+                else -> {
+                }
             }
         }
     }
